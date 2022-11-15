fb2a5c5c1f2d0b0fbf563ad754fcd338fcec4a2e
Antonio Furtado <antoniordf@hotmail.com>
2022-11-15 15:29:13 +0000

    finished task manager app without styling

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def show
+    @task = find_task
+  end
+
+  def edit
+    @task = find_task
+  end
+
+  def update
+    @task = find_task
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = find_task
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    Task.find(params[:id])
+  end
+end
